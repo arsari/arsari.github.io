1d9916274c515f769bbdac4ab7eecb3be3825020
Arturo Santiago-Rivera <asantiago@arsari.com>
2017-04-11 10:53:55 -0400

    Complete change of web page

--- Readme.md
+++ Readme.md
@@ -1,8 +1,9 @@
-Author:
-Arturo Santiago-Rivera
+#Web Development Portfolio
+##Arturo Santiago-Rivera
+_PO Box 1814
+Sabana Seca, PR 00952
+787-602-5496
+asantiago@arsari.com_
 
-Project:
-Web Development Portfolio
-
-Description:
-Arsari web development personal page and coding projects portfolio as a demonstration of coding knowledge and capacity to work on teams of programmers or as a freelance consultant in the production of web pages based on HTML, CSS, and JavaScript coding.
+###Description:
+Personal web devellopment and coding projects portfolio as a demonstration of coding and programming knowledge to work on team of programmers or as a freelance consultant in the development of web sites and web apps based on HTML, CSS, jQuery, JavaScript and Python.
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,171 +1,360 @@
 $(document).ready(function() {
 
-  $(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+    // typing script
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
 
-  $(function() {
-    var ctx1 = document.getElementById("designChart");
-    var ctx2 = document.getElementById("htmlChart");
-    var ctx3 = document.getElementById("jsChart");
-    var ctx4 = document.getElementById("bootChart");
-    var ctx5 = document.getElementById("osChart");
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
 
-    var designChart = new Chart(ctx1, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "Sketch Wireframe"
-        ],
-        datasets: [{
-          data: [80, 20],
-          backgroundColor: [
-            "#E53935",
-            "#f5f5f5"
-          ],
-          hoverBackgroundColor: [
-            "#E53935",
-            "#f5f5f5"
-          ]
-        }]
-      },
-      options: {
-        legend: false,
-        title: {
-          display: true,
-          text: 'Sketch Wireframe',
-          fontSize: 18,
-          fontStyle: 'Bold'
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
-      },
-      animation: {
-        animateScale: true
-      }
-    });
-    var htmlChart = new Chart(ctx2, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "HTML / CSS"
-        ],
-        datasets: [{
-          data: [95, 5],
-          backgroundColor: [
-            "#D81B60",
-            "#f5f5f5"
-          ],
-          hoverBackgroundColor: [
-            "#D81B60",
-            "#f5f5f5"
-          ]
-        }]
-      },
-      options: {
-        legend: false,
-        title: {
-          display: true,
-          text: 'HTML / CSS',
-          fontSize: 18,
-          fontStyle: 'Bold'
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
         }
-      },
-      animation: {
-        animateScale: true
-      }
-    });
-    var jsChart = new Chart(ctx3, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "Javascript"
-        ],
-        datasets: [{
-          data: [60, 40],
-          backgroundColor: [
-            "#8E24AA",
-            "#f5f5f5"
-          ],
-          hoverBackgroundColor: [
-            "#8E24AA",
-            "#f5f5f5"
-          ]
-        }]
-      },
-      options: {
-        legend: false,
-        title: {
-          display: true,
-          text: 'Javascript',
-          fontSize: 18,
-          fontStyle: 'Bold'
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
         }
-      },
-      animation: {
-        animateScale: true
-      }
-    });
-    var bootChart = new Chart(ctx4, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "Bootstrap Framework"
-        ],
-        datasets: [{
-          data: [90, 10],
-          backgroundColor: [
-            "#5E35B1",
-            "#f5f5f5"
-          ],
-          hoverBackgroundColor: [
-            "#5E35B1",
-            "#f5f5f5"
-          ]
-        }]
-      },
-      options: {
-        legend: false,
-        title: {
-          display: true,
-          text: 'Bootstrap Framework',
-          fontSize: 18,
-          fontStyle: 'Bold'
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typing');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
         }
-      },
-      animation: {
-        animateScale: true
-      }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typing > .wrap { border-right: 0.08em solid #e8f40d}";
+        document.body.appendChild(css);
+    };
+
+    // tooltip script call
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
     });
-    var osChart = new Chart(ctx5, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "Windows / OS X"
-        ],
-        datasets: [{
-          data: [95, 5],
-          backgroundColor: [
-            "#00897B",
-            "#f5f5f5"
-          ],
-          hoverBackgroundColor: [
-            "#00897B",
-            "#f5f5f5"
-          ]
-        }]
-      },
-      options: {
-        legend: false,
-        title: {
-          display: true,
-          text: 'Windows / OS X',
-          fontSize: 18,
-          fontStyle: 'Bold'
-        }
-      },
-      animation: {
-        animateScale: true
-      }
+
+    // chart script data
+    $(function() {
+        var ctx1 = $('#designChart');
+        var ctx2 = $('#htmlChart');
+        var ctx3 = $('#cssChart');
+        var ctx4 = $('#jqChart');
+        var ctx5 = $('#jsChart');
+        var ctx6 = $('#bootChart');
+        var ctx7 = $('#winChart');
+        var ctx8 = $('#osxChart');
+
+        var designChart = new Chart(ctx1, {
+            type: 'doughnut',
+            data: {
+                labels: ["Sketch"],
+                datasets: [
+                    {
+                        data: [
+                            75, 25
+                        ],
+                        backgroundColor: [
+                            '#f44336', '#f5f5f5'
+                        ],
+                        hoverBackgroundColor: ['#f44336', '#f5f5f5']
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'Design w/ Sketch',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var htmlChart = new Chart(ctx2, {
+            type: "doughnut",
+            data: {
+                labels: ["HTML"],
+                datasets: [
+                    {
+                        data: [
+                            95, 5
+                        ],
+                        backgroundColor: [
+                            "#9c27b0", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#9c27b0", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'HTML5',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var cssChart = new Chart(ctx3, {
+            type: "doughnut",
+            data: {
+                labels: ["CSS"],
+                datasets: [
+                    {
+                        data: [
+                            95, 5
+                        ],
+                        backgroundColor: [
+                            "#3f51b5", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#3f51b5", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'CSS3',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var jqChart = new Chart(ctx4, {
+            type: "doughnut",
+            data: {
+                labels: ["jQuery"],
+                datasets: [
+                    {
+                        data: [
+                            60, 40
+                        ],
+                        backgroundColor: [
+                            "#03a9f4", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#03a9f4", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'jQuery',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var jsChart = new Chart(ctx5, {
+            type: "doughnut",
+            data: {
+                labels: ["Javascript"],
+                datasets: [
+                    {
+                        data: [
+                            80, 20
+                        ],
+                        backgroundColor: [
+                            "#009688", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#009688", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'Javascript',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var bootChart = new Chart(ctx6, {
+            type: "doughnut",
+            data: {
+                labels: ["Bootstrap"],
+                datasets: [
+                    {
+                        data: [
+                            95, 5
+                        ],
+                        backgroundColor: [
+                            "#ffc107", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#ffc107", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'Bootstrap Framework',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var winhart = new Chart(ctx7, {
+            type: "doughnut",
+            data: {
+                labels: ["Windows"],
+                datasets: [
+                    {
+                        data: [
+                            95, 5
+                        ],
+                        backgroundColor: [
+                            "#ff5722", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#ff5722", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'Windows',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
+        var osxhart = new Chart(ctx8, {
+            type: "doughnut",
+            data: {
+                labels: ["macOS"],
+                datasets: [
+                    {
+                        data: [
+                            95, 5
+                        ],
+                        backgroundColor: [
+                            "#795548", "#f5f5f5"
+                        ],
+                        hoverBackgroundColor: ["#795548", "#f5f5f5"]
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    padding: 10,
+                    text: 'macOS',
+                    fontSize: 18,
+                    fontStyle: 'Bold',
+                    fontColor: '#000000'
+                }
+            },
+            animation: {
+                animateScale: true
+            }
+        });
     });
-  });
+
+    // flip boxes
+    $('.hover').hover(function() {
+        $(this).addClass('flip');
+    }, function() {
+        $(this).removeClass('flip');
+    });
 
 });
